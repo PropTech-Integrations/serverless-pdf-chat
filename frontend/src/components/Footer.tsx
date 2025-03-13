b5dd0039415f65bc40984e2da7c0461be6148ac1
Daniel Holmlund <agnathan@gmail.com>
2025-03-12 20:50:23 -0700

    Remove footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
   return (
     <div className="bg-gray-100 mt-auto">
       <footer className="container">
-        <div className=" flex flex-row justify-between py-3 text-sm">
+        {/* <div className=" flex flex-row justify-between py-3 text-sm">
           <div className="inline-flex items-center">
             <CloudIcon className="w-5 h-5 mr-1.5" />
             Powered by Amazon Web Services
@@ -21,7 +21,7 @@ const Footer: React.FC = () => {
               Source code on GitHub
             </a>
           </div>
-        </div>
+        </div> */}
       </footer>
     </div>
   );
